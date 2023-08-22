ba46dda79e2f566ca65170301e585a78abc7f1de
jtscripta <87989965+jtscripta@users.noreply.github.com>
2023-08-22 15:01:06 -0400

    test

--- app-v1.py
+++ app-v1.py
@@ -2,6 +2,8 @@ __import__('pysqlite3')
 import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
+import sqlite3
+
 import streamlit as st
 from langchain.llms import OpenAI
 from langchain.text_splitter import CharacterTextSplitter
