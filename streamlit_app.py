9c01ab692824183117c185b7e5ac7b5e408453b1
jtscripta <87989965+jtscripta@users.noreply.github.com>
2023-08-22 15:49:37 -0400

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ def generate_response(uploaded_file, openai_api_key, query_text):
         # Create retriever interface
         retriever = db.as_retriever()
         # Create QA chain
-        qa = RetrievalQA.from_chain_type(llm=OpenAI(openai_api_key=openai_api_key), chain_type='stuff', retriever=retriever)
+        qa = RetrievalQA.from_chain_type(llm=OpenAI(openai_api_key=openai_api_key), chain_type='stuff', retriever=retriever, max_tokens_limit=4097)
         return qa.run(query_text)
 
 
